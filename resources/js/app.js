3b5fa72c5676bd52c533d9efe8e4797c5d586eec
Phyo Thiha <phyothiha96@gmail.com>
2022-09-17 11:38:05 +0630

    chore: install laravel ui and vue ui framework prime

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import Alpine from "alpinejs";
+window.Alpine = Alpine;
+
+import { createApp } from "vue";
+
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/saga-blue/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+import "primeflex/primeflex.css";
+
+import Calendar from "primevue/calendar";
+
+// Creating an Application
+const app = createApp({});
+
+// Installing Plugins
+app.use(PrimeVue);
+
+// Registration Global Components
+app.component("Calendar", Calendar);
+
+// Mounting the App
+app.mount("#app");
+
+Alpine.start();
